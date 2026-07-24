0b1699c9bde7f3191911c62b2857515dcb6ca62c
czagoh <dorian.pennegat@hotmail.fr>
2026-07-24 10:59:39 +0200

    Feat/00-initialization

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,13 +5,18 @@ import "dotenv/config";
 // Note: This is optional and can be removed if the database connection
 // is not required when starting the application
 import "../database/checkConnection";
-
+import type { RequestHandler } from "express";
 // Import the Express application from ./app
 import app from "./app";
 
 // Get the port from the environment variables
 const port = process.env.APP_PORT;
 
+const sayWelcome: RequestHandler = (req, res) => {
+  res.send("Welcome to the best website for series !");
+};
+
+app.get("/", sayWelcome);
 // Start the server and listen on the specified port
 app
   .listen(port, () => {
