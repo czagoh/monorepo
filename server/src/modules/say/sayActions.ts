21432f3c652464da8a4c80dc92509553bda14b87
czagoh <dorian.pennegat@hotmail.fr>
2026-07-27 15:43:22 +0200

    Monorepo-04

--- server/src/modules/say/sayActions.ts
+++ server/src/modules/say/sayActions.ts
@@ -1,6 +1,7 @@
 import type { RequestHandler } from "express";
 
 const sayWelcome: RequestHandler = (req, res) => {
+  console.info(req.query);
   res.send("Welcome to the best website for series !");
 };
 
