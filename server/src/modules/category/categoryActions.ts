03a7fe4caedfd245c960e0bff7208318ad6e7c0d
czagoh <dorian.pennegat@hotmail.fr>
2026-07-30 11:12:11 +0200

    Monorepo-06

--- server/src/modules/category/categoryActions.ts
+++ server/src/modules/category/categoryActions.ts
@@ -1,3 +1,4 @@
+import categoryRepository from "./categoryRepository";
 // Some data to make the trick
 
 const categories = [
@@ -15,8 +16,9 @@ const categories = [
 
 import type { RequestHandler } from "express";
 
-const browse: RequestHandler = (req, res) => {
-  res.json(categories);
+const browse: RequestHandler = async (req, res) => {
+  const categoriesFromDB = await categoryRepository.readAll();
+  res.json(categoriesFromDB);
 };
 
 const read: RequestHandler = (req, res) => {
---
+++ server/src/modules/program/programRepository.ts
@@ -0,0 +1,25 @@
+import databaseClient from "../../../database/client";
+
+import type { Result, Rows } from "../../../database/client";
+
+type Program = {
+  id: number;
+  title: string;
+  synopsis: string;
+  poster: string;
+  country: string;
+  year: number;
+  category_id: number;
+};
+
+class ProgramRepository {
+  async readAll() {
+    // Execute the SQL SELECT query to retrieve all categories from the "program" table
+    const [rows] = await databaseClient.query<Rows>("select * from program");
+
+    // Return the array of prog
+    return rows as Program[];
+  }
+}
+
+export default new ProgramRepository();
